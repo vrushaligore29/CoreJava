dd2131c70e5d24aa793aa3baeb23479288d8b0d2
Techsum3 <Techsum3@Techsum3-PC>
2019-08-10 16:46:50 +0530

    Added another statement

--- CoreJava/src/com/corejava/programpractice/HelloWorld.java
+++ CoreJava/src/com/corejava/programpractice/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Automation Testing!!");
+		System.out.println("Welcome to Automation Testing Class!!");
 	}
 }
